bf631d27a0108acbc9d405a047447f7bf80d1635
VictorAburtoDiaz <victoraburtodiaz@gmail.com>
2024-11-08 09:55:45 -0300

    Añadidas imágenes de agentes y código actualizado

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 // Lista de agentes actuales en VALORANT
-const agentes = [
-    "Astra", "Breach", "Brimstone", "Chamber", "Cypher",
-    "Fade", "Harbor", "Jett", "KAY/O", "Killjoy",
-    "Neon", "Omen", "Phoenix", "Raze", "Reyna",
-    "Sage", "Skye", "Sova", "Viper", "Yoru", "Deadlock", "Iso","Clove","Vyse"
-  ];
-  
+const agentes = {
+  "Astra": "images/Astra.jpg",
+  "Breach": "images/Breach.jpg",
+  "Brimstone": "images/Brimstone.jpg",
+  "Chamber": "images/Chamber.jpg",
+  "Cypher": "images/Cypher.jpg",
+  "Fade": "images/Fade.jpg",
+  "Harbor": "images/Harbor.jpg",
+  "Jett": "images/Jett.jpg",
+  "KAY/O": "images/Kayo.jpg",
+  "Killjoy": "images/Killjoy.jpg",
+  "Neon": "images/Neon.jpg",
+  "Omen": "images/Omen.jpg",
+  "Phoenix": "images/Phoenix.jpg",
+  "Raze": "images/Raze.jpg",
+  "Reyna": "images/Reyna.jpg",
+  "Sage": "images/Sage.jpg",
+  "Skye": "images/Skye.jpg",
+  "Sova": "images/Sova.jpg",
+  "Viper": "images/Viper.jpg",
+  "Iso": "images/Iso.jpg",
+  "Clove": "images/Clove.jpg",
+  "Vyse": "images/Vyse.jpg",
+  "Yoru": "images/Yoru.jpg"
+};
   // Función para seleccionar un agente aleatorio
   function mostrarMensaje() {
     const indiceAleatorio = Math.floor(Math.random() * agentes.length);
